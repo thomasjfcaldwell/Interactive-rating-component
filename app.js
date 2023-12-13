1f8365c900615b2203580eeab8cef7c12a2277d0
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2023-12-12 16:30:08 -0800

    start off

---
+++ app.js
@@ -0,0 +1,8 @@
+const submitButton = document.querySelector('.rating_card_button');
+const ratingCard = document.querySelector('.rating_card');
+const stateCard = document.querySelector('.state_card');
+
+submitButton.onclick = function () {
+	ratingCard.style.display = 'none';
+	stateCard.style.display = 'block';
+};
